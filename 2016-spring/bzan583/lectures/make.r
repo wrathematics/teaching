aeed11ea32e3a4fd635454459ebe400f98f5a946
wrathematics <wrathematics@gmail.com>
2016-01-21 10:41:01 -0500

    first pass make

---
+++ 2016-spring/bzan583/lectures/make.r
@@ -0,0 +1,53 @@
+library(rmarkdown)
+
+clean <- function(outputs=TRUE)
+{
+  files <- dir()[grep(dir(), pattern="([.]md|[.]log|[.]tex)")]
+  if (outputs)
+    files <- c(files, dir()[grep(dir(), pattern="([.]pdf|[.]html)")])
+  
+  file.remove(files)
+  invisible()
+}
+
+make_slides <- function(files)
+{
+  for (file in files)
+    rmarkdown::render(file)
+  
+  invisible()
+}
+
+make_handouts <- function(files)
+{
+  newoutput <- "output:
+  pdf_document:
+    toc: true
+    toc_depth: 2
+"
+  
+  for (file in files)
+  {
+    txt <- readLines(file)
+    start <- which(grepl(txt, pattern="output:"))
+    end <- which(grepl(txt, pattern="^---"))[2] - 1
+    txt[start] <- newoutput
+    txt <- paste0(txt[-((start+1):end)], collapse="\n")
+    
+    newfile <- sub(x=file, pattern="[.]", replacement="_handout.")
+    writeLines(txt, newfile)
+    
+    rmarkdown::render(file)
+    
+    file.remove(newfile)
+  }
+  
+  clean(outputs=FALSE)
+}
+
+
+
+files <- dir()[grep(dir(), pattern="[.]Rmd")]
+clean()
+make_slides(files)
+make_handouts(files)
